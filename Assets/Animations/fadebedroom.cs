38dff746ef16f4714c891c8822dfa85c3c7610df
DESKTOP-2DD5ONT\user <baraedev81@gmail.com>
2025-03-05 19:48:08 +0000

    added the finalcutsne and a second jumpcare(not added yet) and the cutscene not finished i also added a show up sfx

---
+++ Assets/Animations/fadebedroom.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class fadebedroom : MonoBehaviour
+{
+    public Image imageComponent; // Assign in Inspector
+    public Animator animator;
+    public AudioSource audioSource;
+    public AudioSource show;
+    public Transform monster;
+    void Start()
+    {
+        StartCoroutine(DisableAfterDelay());
+    }
+
+    IEnumerator DisableAfterDelay()
+    {
+        yield return new WaitForSeconds(1f); // Wait for 1 seconds
+        imageComponent.enabled = false; // Disable the Image component
+        animator.enabled = true;
+        
+        StartCoroutine(DisableAfterDelaye());
+    }
+    IEnumerator DisableAfterDelaye()
+    {
+        yield return new WaitForSeconds(0.15f); // Wait for 0.15 seconds
+        audioSource.enabled = true;
+        StartCoroutine(DisableAfterDelayee());
+
+    }
+    IEnumerator DisableAfterDelayee()
+    {
+        yield return new WaitForSeconds(4f); // Wait for 2 seconds
+        monster.position = new Vector3(0f, 403.161f, 0.495f);
+        show.Play();
+    }
+}
