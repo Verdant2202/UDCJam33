f23a85a5f4acf33213779900b7baa99d2ced6d52
DESKTOP-2DD5ONT\user <baraedev81@gmail.com>
2025-03-06 16:13:50 +0000

    added custom fog (not looking good so i dished it) and added a main menu (not finished yet) and also added some fonts

---
+++ Assets/Main Menu/ButtonsHander.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+public class ButtonsHander : MonoBehaviour
+{
+
+public void New_Game()
+    {
+
+        SceneManager.LoadScene("ForestScene");
+    }
+
+}
