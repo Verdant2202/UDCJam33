aeabe3bb3a0ec2744414412c178edff52ce71fd2
VerdantGMD <cverdantgmdc@gmail.com>
2025-03-08 22:47:27 +0100

    trying to fix git lol

---
+++ Assets/Scripts/PlayerFootstepsManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFootstepsManager : MonoBehaviour
+{
+    [SerializeField] FirstPersonController movement;
+
+    [SerializeField] SFXSO walkSound;
+    [SerializeField] SFXSO sprintSound;
+    [SerializeField] AudioSource walkAudioSource;
+    [SerializeField] AudioSource sprintAudioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        walkAudioSource.clip = walkSound.clip;
+        sprintAudioSource.clip = sprintSound.clip;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        walkAudioSource.volume = walkSound.naturalVolume * SFXManager.Instance.globalSFXVolume;
+        sprintAudioSource.volume = sprintSound.naturalVolume * SFXManager.Instance.globalSFXVolume;
+        if (movement.isSprinting)
+        {
+            sprintAudioSource.enabled = true;
+            walkAudioSource.enabled = false;
+        }
+        else if(movement.isWalking)
+        {
+            sprintAudioSource.enabled = false;
+            walkAudioSource.enabled = true;
+        }
+        else
+        {
+            walkAudioSource.enabled = false;
+            sprintAudioSource.enabled = false;
+        }
+
+        if (Time.timeScale == 0f)
+        {
+            walkAudioSource.enabled = false;
+            sprintAudioSource.enabled = false;
+        }
+    }
+}
